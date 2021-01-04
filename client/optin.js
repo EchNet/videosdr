68a54a7934e102966bccc72b984976f220d78470
James Echmalian <ech@ech.net>
2021-01-04 15:15:00 -0500

    Show/hide popup.

--- client/optin.js
+++ client/optin.js
@@ -7,14 +7,24 @@
     return scripts[scripts.length - 1];
   })();
 
+  // Polyfill for Array.prototype.forEach:
+  if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this);
+      }
+    }
+  }
+
   // Get options.
   const options = (function(defaultOptions) {
     const queryVars = currentScript.src.replace(/^[^\?]+\??/, "").split("&");
     const queryStringOptions = {};
-    for (var i = 0; i < queryVars.length; ++i) {
-      const pair = queryVars[i].split("=");
+    queryVars.forEach(function(qv) {
+      const pair = qv.split("=");
       queryStringOptions[pair[0]] = decodeURI(pair[1]).replace(/\+/g, ' ');
-    }
+    })
     const options = {};
     for (var key in defaultOptions) {
       options[key] = currentScript.getAttribute("data-" + key)
@@ -26,78 +36,164 @@
     salt: ""
   });
 
-  var prereqs = {};
+  function isIOS() {
+    return !!/iphone|ipod|ipad/i.test(userAgent) }
 
-  function init() {
-    // Bring in required Javascripts.
-    (function(scripts) {
-      for (var i = 0; i < scripts.length; ++i) {
-        (function(ss) {
-          var script = document.createElement("script");
-          script.src = ss.src;
-          script.addEventListener("load", function() {
-            markPrereq(ss.name);
-          });
-          document.head.appendChild(script)
-        })(scripts[i])
-      }
-    })([
+  function initialize() {
+    executeInstruction([
       {
-        name: "mustache",
-        src: "https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js"
+        "code": "appendStyle",
+        "style": [
+          "div.optin-modal-screen {",
+            "position: fixed;",
+            "top: 0;",
+            "left: 0;", 
+            "width: 100%;", 
+            "height: 100%;", 
+            "background: rgba(192,192,192,0.5);", 
+            "z-index: 2000000;",
+          "}"
+        ].join("\n")
+      },
+      {
+        "code": "appendStyle",
+        "style": [
+          "div.optin-modal-frame {",
+            "position: fixed;",
+            "width: 100%;",
+            "max-width: 480px;",
+            "max-height: 360px;", 
+            "margin: auto;",
+            "background: white;",
+            "z-index: 2000001;",
+          "}"
+        ].join("\n")
+      },
+      {
+        "code": "createPopup",
+        "name": "popup1",
+        "html": "<div><h2>POPUP1</h2></div>"
       }
     ])
   }
 
-  // DOM utility.
-  function forEachElementOfClass(className, func) {
-    var elements = document.getElementsByClassName(className);
-    for (var i = 0; i < elements.length; ++i) {
-      func(elements[i]);
+  const INSTRUCTION_DISPATCH = {
+    appendStyle: function(instruction) {
+      appendStyle(instruction.style);
+    },
+    appendHtml: function(instruction) {
+      appendHtml(instruction.selector, instruction.html)
+    },
+    addClass: function(instruction) {
+      addClass(instruction.selector, instruction["class"])
+    },
+    removeClass: function(instruction) {
+      removeClass(instruction.selector, instruction["class"])
+    },
+    createPopup: function(instruction) {
+      createPopup(instruction.name, instruction.html)
+    },
+    handleClick: function(instruction) {
+      handleClick(instruction.target, instruction.action)
     }
   }
 
-  // DOM utility.
-  function visitDescendantTextNodes(root, func) {
-    for (var child = root.firstChild; child; child = child.nextSibling) {
-      if (child.nodeType == 3 /* text */) {
-        func(child);
+  function executeInstruction(instruction) {
+    if (instruction) {
+      if (Array.isArray(instruction)) {
+        instruction.forEach(executeInstruction)
       }
       else {
-        visitDescendantTextNodes(child, func);
+        INSTRUCTION_DISPATCH[instruction.code](instruction)
       }
     }
   }
 
-  function isIOS() {
-    return !!/iphone|ipod|ipad/i.test(userAgent)
+  function appendHtml(selector, html) {
+    var parent = selector ? document.querySelector(selector) : document.body;
+    parent.innerHTML += html;
   }
 
-  function initialize(params) {
-    return 1;
+  function addClass(selector, _class) {
+    var ele = selector ? document.querySelector(selector) : document.body;
+    ele.classList.add(_class);
   }
 
-  // Data and code are ready.  Initialize the video and display custom text.
-  function startUp(params) {
-    if (!initialize(params)) {
-      // If initialization is not yet possible, watch for DOM mutations.
-      new MutationObserver(function(mutList, observer) {
-        if (initialize(params)) {
-          observer.disconnect();
+  function removeClass(selector, _class) {
+    var ele = selector ? document.querySelector(selector) : document.body;
+    ele.classList.remove(_class);
+  }
+
+  function createPopup(name, html) {
+    executeInstruction([
+      {
+        "code": "appendHtml",
+        "html": "<div class='optin-modal-element optin-modal-screen " + name + "'></div>"
+      },
+      {
+        "code": "appendHtml",
+        "html": [
+          "<div class='optin-modal-element optin-modal-frame " + name + "'>",
+            "<div class='optin-close-container'>",
+              "<button>X</button>",
+            "</div>",
+          "</div>"
+        ].join("")
+      },
+      {
+        "code": "appendHtml",
+        "selector": "div.optin-modal-frame",
+        "html": html
+      },
+      {
+        "code": "appendStyle",
+        "style": [
+          "body:not(." + name + "-shown) .optin-modal-element." + name + " {",
+            "display: none;",
+          "}"
+        ].join(" ")
+      },
+      {
+        "code": "handleClick",
+        "target": "a[href='#" + name + "']",
+        "action": {
+          "code": "addClass",
+          "class": name + "-shown"
         }
-      }).observe(document.getElementsByTagName("body")[0], {
-        childList: true
-      })
-    }
+      },
+      {
+        "code": "handleClick",
+        "target": "div.optin-modal-screen." + name,
+        "action": {
+          "code": "removeClass",
+          "class": name + "-shown"
+        }
+      },
+      {
+        "code": "handleClick",
+        "target": "div.optin-modal-frame." + name + " div.optin-close-container", 
+        "action": {
+          "code": "removeClass",
+          "class": name + "-shown"
+        }
+      }
+    ])
   }
 
-  // Gate startup on required conditions.
-  function markPrereq(name, value) {
-    if (!prereqs[name]) {
-      prereqs[name] = value || 1;
-      if (prereqs["page"]) {
-        startUp(prereqs["params"])
-      }
+  function appendStyle(style) {
+    var head = document.getElementsByTagName('head')[0];
+    var styles  = document.createElement('style');
+    styles.appendChild(document.createTextNode(style));
+    head.appendChild(styles);
+  }
+
+  function handleClick(target, action) {
+    var links = document.querySelectorAll(target);
+    for (var i = 0; i < links.length; ++i) {
+      links[i].addEventListener("click", function(e) {
+        e.preventDefault();
+        executeInstruction(action);
+      })
     }
   }
 
@@ -122,6 +218,5 @@
   }
 
   // Main.
-  init();
-  whenPageLoaded(function() { markPrereq("page") })
+  whenPageLoaded(initialize)
 })();
