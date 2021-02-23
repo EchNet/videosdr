69472e69424999cbaac229c497df7a3d18917709
James Echmalian <ech@ech.net>
2021-02-23 16:57:22 -0500

    Fix query - use key query, not full table scan.

--- dataservice/getVideoParams.js
+++ dataservice/getVideoParams.js
@@ -6,13 +6,13 @@ const dynamodb = new AWS.DynamoDB();
 exports.handler = async(event, ctx, callback) => {
   var params = {
      TableName: "VideoParams",
-     FilterExpression: '#key = :key',
+     KeyConditionExpression: '#key = :key',
      ExpressionAttributeValues: { ":key": event.key },
      ExpressionAttributeNames: { "#key": "key" }
   };
   const promise = new Promise(function(resolve, reject) {
     const docClient = new AWS.DynamoDB.DocumentClient();
-    docClient.scan(params, function(err, data) {
+    docClient.query(params, function(err, data) {
       if (err) {
         reject(err);
       }
